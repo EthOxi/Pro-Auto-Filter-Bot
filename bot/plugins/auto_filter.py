d1c2a3dc76fcc6b75b6ee13b02bf6a267d23dd2f
staneswilson <82250240+staneswilson@users.noreply.github.com>
2021-08-05 14:21:46 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -153,8 +153,11 @@ async def auto_filter(bot, update):
         
         # Just A Decaration
         result[0].append([
-            InlineKeyboardButton(f"🔰 Page 1/{len_result if len_result < max_pages else max_pages} 🔰", callback_data="ignore")
+            InlineKeyboardButton(f"⭕Page 1/{len_result if len_result < max_pages else max_pages} ⭕", callback_data="ignore")
         ])
+        result[0].append([
+        InlineKeyboardButton('📷Instagram📷', url='https://www.instagram.com/movie___willa')
+    ])
         
         
         # if show_invite is True Append invite link buttons
